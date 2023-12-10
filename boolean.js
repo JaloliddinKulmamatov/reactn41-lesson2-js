2ba9307bec7c8a2a42d1808ccf7980e3a38fbfdd
JaloliddinKulmamatov <itjalol2010@gmail.com>
2023-12-11 04:56:28 +0500

    jalol

---
+++ boolean.js
@@ -0,0 +1,42 @@
+//Boolean 1
+
+// let A = 6;
+
+// console.log(A % 2 = 0);
+
+// Boolean 2 
+
+
+// let A = 3;
+// let B = 6;
+// console.log(A >= 0,  B < -3);
+
+// Boolean 3
+
+// let A = 4;
+// let B = 2;
+// let C = -2;
+
+// console.log(A <= B <= C);
+
+// Boolean5 
+
+// let A = 4;
+// let B = 5;
+
+// console.log(A  % 2 == 1, B  % 2 == 1);
+
+//Boolean 6
+
+// let A = 4;
+// let B = 5;
+
+// console.log(A  % 2 == 1, B  % 2 == 1);
+
+//Boolean 7
+
+// let A = 2;
+// let B = -1;
+// let C = 5;
+
+// console.log(A > 0,B > 0,C > 0,);
