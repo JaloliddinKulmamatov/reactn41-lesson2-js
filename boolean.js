ea94cb5f1a2adc6ec6309c4f0ca71e5e63815140
JaloliddinKulmamatov <itjalol2010@gmail.com>
2023-12-13 14:55:37 +0500

    jalol

--- boolean.js
+++ boolean.js
@@ -33,10 +33,45 @@
 
 // console.log(A  % 2 == 1, B  % 2 == 1);
 
-//Boolean 7
+// Boolean 7
+// let A = 4;
+// let B = -8;
+// let C = 1;
 
-// let A = 2;
-// let B = -1;
-// let C = 5;
+// let D = A > 0 && B > 0 && C > 0;
 
-// console.log(A > 0,B > 0,C > 0,);
+
+// console.log(D > 0,);
+
+//Boolean8
+
+// let A = -1;
+// let B = 8;
+// let C = -1;
+
+// let D = A < 0 || B < 0 || C < 0;
+
+// console.log(D );
+
+//boolean10 
+
+// let A = 22;
+// D =  A < 100, A > 9 
+
+// console.log( D && A % 2 == 0);
+
+
+//boolean11
+
+// let A = 228;
+// D =  A < 1000, A > 99
+
+// console.log( D && A % 2 == 1);
+
+//boolean12
+
+let A = 1;
+let B = 5;
+let C = -5;
+
+console.log(Math.sign(A == B || B == C || C == A ));
